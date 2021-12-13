33e7c0ef4381413b67ea193914d9b3277cd884a4
dijair12 <dijair.ribeiro@aluno.faculdadeimpacta.com.br>
2021-12-13 06:19:50 -0300

    change state for search

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -5,14 +5,14 @@ const CardList = ({ robots }) => {
   return (
     <div>
       {
-        robots.map((user, i) => {
+        robots?.map((user, i) => {
           return (
             <Card
               key={i}
               id={robots[i].id}
               name={robots[i].name}
               email={robots[i].email}
-              />
+            />
           );
         })
       }
